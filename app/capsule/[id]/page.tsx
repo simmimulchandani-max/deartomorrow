bcb6b4a1825d097d55c763798be0816de0c91fe5
Simmi Mulchandani <simmi.mulchandani@gmail.com>
2026-03-31 21:32:17 -0400

    Add polaroid UI with confetti and sound

--- app/capsule/[id]/page.tsx
+++ app/capsule/[id]/page.tsx
@@ -1,5 +1,9 @@
 'use client';
-
+type Memory = {
+  id: string;
+  message: string;
+  name?: string;
+};
 import UnlockCelebration from '@/components/UnlockCelebration';
 
 export default function CapsulePage({ memories, unlocked }: { memories: Memory[], unlocked: boolean }) {
